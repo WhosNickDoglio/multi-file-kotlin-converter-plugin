95829a07e38c1c5c686d7db4c66b09682aa94745
nicholasdoglio <NicholasDoglio@Gmail.com>
2025-02-17 22:33:40 -0500

    Convert build.gradle.kts to Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,13 +6,13 @@ buildscript {
 }
 
 plugins {
-    id 'org.jetbrains.kotlin.jvm' version '1.8.10'
-    id 'org.jetbrains.intellij' version '1.17.4'
-    id "io.gitlab.arturbosch.detekt" version "1.9.1"
+    id("org.jetbrains.kotlin.jvm") version "1.8.10"
+    id("org.jetbrains.intellij") version "1.17.4"
+    id("io.gitlab.arturbosch.detekt") version "1.9.1"
 }
 
-group 'com.pandora.plugin'
-version '0.4.3'
+group = "com.pandora.plugin"
+version = "0.4.3"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_11
@@ -21,21 +21,21 @@ java {
 
 
 
-tasks.patchPluginXml {
-    changeNotes = """
+tasks.patchPluginXml.configure {
+    changeNotes.set("""
     <ul>
       <li>0.1: Initial version of the Multiple File Kotlin Converter plugin for IntelliJ IDEA.</li>
       <li>0.2: Fixing small issues found in testing with cancellation and layout.</li>
       <li>0.3: Save commit message during a single session.</li>
       <li>0.4: Fix Deprecated project root usage.</li>
     </ul>
-    """
+    """)
 }
 
 intellij {
-    version = "2019.2"
-    updateSinceUntilBuild = false
-    plugins = ["java"]
+    version.set("2019.2")
+    updateSinceUntilBuild.set(false)
+    plugins.set(listOf("java"))
     instrumentCode.set(false)
 }
 
@@ -44,7 +44,7 @@ repositories {
 }
 
 dependencies {
-    testImplementation group: 'junit', name: 'junit', version: '4.12'
+    testImplementation("junit:junit:4.13.2")
 }
 
 tasks.runIde.configure {
@@ -52,17 +52,17 @@ tasks.runIde.configure {
 }
 
 
-tasks.publishPlugin {
-    token = project.properties['jetbrains.publish.token']
+tasks.publishPlugin.configure {
+    token.set(project.properties["jetbrains.publish.token"] as? String)
 }
 
-tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile).configureEach {
+tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class).configureEach {
     kotlinOptions.jvmTarget = "11"
 }
 
-apply plugin: 'io.gitlab.arturbosch.detekt'
-
-tasks.detekt.jvmTarget = "1.8"
+tasks.detekt.configure {
+    jvmTarget = "1.8"
+}
 
 detekt {
     parallel = true
@@ -71,5 +71,5 @@ detekt {
 }
 
 dependencies {
-    detektPlugins "io.gitlab.arturbosch.detekt:detekt-formatting:1.9.1"
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.9.1")
 }
