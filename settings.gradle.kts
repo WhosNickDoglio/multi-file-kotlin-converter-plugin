b6e6d8ac04dc4012a83efca4fd81ce03cdfb4972
nicholasdoglio <NicholasDoglio@Gmail.com>
2025-02-17 23:07:59 -0500

    Add settings plugins for Develocity and Foojay

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,3 +12,16 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
+
+plugins {
+    id("com.gradle.develocity") version "3.19.1"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "2.1"
+}
+
+develocity {
+    buildScan {
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree = "yes"
+    }
+}
